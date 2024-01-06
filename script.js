2cf9172a7e835398e0fcdfc05f94d8550b2d8dbc
faridibrahimov <83863119+faridibrahimov@users.noreply.github.com>
2024-01-06 14:57:54 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+$('.carousel').slick({
+    prevArrow: '<img src="images/Arrow_left.svg" width="100px" height="100px">',
+    nextArrow: '<img src="images/Arrow_right.svg" width="100px" height="100px">',
+});
